6ef809f9c2b76138f6bcfcb46d89d8f260cf7096
amimanye <amimanyeaglago@gmail.com>
2024-08-08 07:33:42 +0000

    Not found and pycodestyle fix

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,19 +2,27 @@
 """
 Starts the Flask web server
 """
-
-from flask import Flask
-from models import storage
 from api.v1.views import app_views
+from flask import Flask, jsonify
+from models import storage
 import os
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
 
 @app.teardown_appcontext
 def teardown_db(exception):
     """Closes storage on teardown"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """Handles 404 errors with a JSON response"""
+    return jsonify({"error": "Not found"}), 404
+
 
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
