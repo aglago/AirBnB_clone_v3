ebf76edd63e4b6cfe89a81329d372072be5e3630
amimanye <amimanyeaglago@gmail.com>
2024-08-08 07:29:31 +0000

    pycodestyle fix

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,10 +5,12 @@ from flask import jsonify
 from api.v1.views import app_views
 from models import storage
 
+
 @app_views.route('/status', methods=['GET'])
 def status():
     """Returns the status of the API"""
     return jsonify({"status": "OK"})
+
 
 @app_views.route('/stats', methods=['GET'])
 def stats():
